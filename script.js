aa05560336f65bde88e28a4e7be6343f3dbcc152
nazmul98 <coder.nazmul@gmail.com>
2021-03-11 12:42:02 -0500

    update dom for manipulating

---
+++ script.js
@@ -0,0 +1,7 @@
+const searchArea = document.getElementById('search-area');
+const searchBtn = document.getElementById('btn-search');
+const weatherIcon = document.getElementById('weather-icon');
+const cityName = document.getElementById('city');
+const temperature = document.getElementById('temperature');
+const weather = document.getElementById('weather');
+
